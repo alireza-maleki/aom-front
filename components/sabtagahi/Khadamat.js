28e3e481e62bee545c43b21ad70b65616dee8ff3
AliMaleki <alimalekinia11@gmail.com>
2023-02-07 23:37:09 -0800

    "set api component khadamat"

--- components/sabtagahi/Khadamat.js
+++ components/sabtagahi/Khadamat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 import classes from "./Khadamat.module.css";
@@ -7,15 +7,17 @@ import classes from "./Khadamat.module.css";
 import axios from 'axios';
 
 
+
 const Khadamat = () => {
 
     // ### Products Group UseState ###
     const [selectData, setSelectData] = useState([]);
     const [postId, setPostId] = useState();
+    const [productId, setProductId] = useState();
 
     // ### Products UseState ###
     const [getCategory, setGetCategory] = useState();
-    const [categoryId, setGetCategoryId] = useState();
+    const [categoryId, setCategoryId] = useState();
 
     // ### Sakht UseState ###
     const [getSakht, setGetSakht] = useState([]);
@@ -32,12 +34,27 @@ const Khadamat = () => {
     // ### Moxo UseState ###
     const [mozo, setMozo] = useState();
 
+    // ### Status Products ###
+    const [statusProduct, setStatusProduct] = useState();
 
+    // ### TextArea ###
+    const [textArea, setTextArea] = useState();
+
+    // ### Input Disabled ###
+    const [showInput, setShowInput] = useState(false);
+
+    // ### Monay Status ###
+    const [monayStatus, setMonayStatus] = useState();
+
+    // ### Product Price ###
+    const [productPrice, setProductPrice] = useState();
 
     // ### Get Prodeucts Group ###
-    const getApiHandler = async () => {
+    const getApiHandler = async (e) => {
 
         // console.log("ali");
+
+        setProductId(e.target.value)
 
         if (selectData.length == 0) {
 
@@ -87,7 +104,7 @@ const Khadamat = () => {
 
     // ### Save Products Data ###
     const categoryOnclick = (e) => {
-        setGetCategoryId(e.target.value)
+        setCategoryId(e.target.value)
     }
 
     // ### Get Contry ###
@@ -121,35 +138,64 @@ const Khadamat = () => {
         const { data } = await axios.get("http://192.168.0.206:1212/advert/v1/brands/");
         setGetBrand(data);
         console.log(data);
-    }
+    };
 
     // ### Save Brand Data ###
     const brandOnchange = (e) => {
         setBrand(e.target.value);
-    }
+    };
 
 
     // ### Save Mozo Data ###
     const mozoOnchange = (e) => {
         setMozo(e.target.value)
+    };
+
+    // ### Save Product Status ###
+    const statusHandler = (e) => {
+        setStatusProduct(e.target.value)
+    };
+
+    // ### Save TextArea ###
+    const textAreaHandler = (e) => {
+        setTextArea(e.target.value)
+    };
+
+    // ### Save Monay Status ###
+    const monayHandler = (e) => {
+        setMonayStatus(e.target.value);
+
+        if (e.target.value == "تومان") {
+            setShowInput(true)
+        } else {
+            setShowInput(false)
+        }
+
+    };
+
+    // ### Save Price ###
+    const priceHandler = (e) => {
+        setProductPrice(e.target.value);
     }
 
+
+    (parseInt(productId));
+    (parseInt(categoryId));
+
     let agahiDetail = {
-        categorys: [selectData, categoryId],
+        categorys: [productId, categoryId],
         title: mozo,
-        description: "ali",
-        agreement_price: "20.2",
+        description: textArea,
+        agreement_price: showInput,
         city: ostan,
-        status_type: "نو"
+        status_type: statusProduct
     }
 
     const submitDataHandler = (e) => {
 
         e.preventDefault();
 
-        axios.post("192.168.0.206:1313/advert/v1/make-advert/", {
-            agahiDetail
-        })
+        axios.post("http://192.168.0.206:1313/advert/v1/make-advert/", agahiDetail)
             .then((response) => {
                 console.log(response);
             })
@@ -157,232 +203,242 @@ const Khadamat = () => {
                 console.log(error);
             });
 
+        setProductId("");
+        setCategoryId("");
+        setMozo("");
+        setTextArea("");
+        setShowInput("");
+        setOstan("");
+        setStatusProduct("");
 
     }
 
     return (
 
-        <div className={classes.khadamt}>
+        <form onSubmit={submitDataHandler}>
 
-            {/* <div className={classes.rightcontrol}>
-                
-            </div> */}
+            <div className={classes.khadamt}>
 
-            <div className={classes.container}>
+                {/* <div className={classes.rightcontrol}>
+    
+</div> */}
 
-                <div className={classes.mozo}>
+                <div className={classes.container}>
 
-                    <label>
-                        موضوع
-                        <span>*</span>
-                    </label>
-
-                    <input type='text' placeholder='موضوع آگهی را ثبت نمایید' onChange={mozoOnchange} required />
-                </div>
-
-                <div className={classes.rightcontrol}>
-
-                    <div className={classes.text}>
+                    <div className={classes.mozo}>
 
                         <label>
-                            گروه کالایی
+                            موضوع
                             <span>*</span>
                         </label>
 
-                        <label>
-                            محصول
-                            <span>*</span>
-                        </label>
-
-                        <label>
-                            ساخت
-                            <span></span>
-                        </label>
-
-
-                        <label>
-                            آدرس
-                            <span></span>
-                        </label>
-
-
+                        <input type='text' placeholder='موضوع آگهی را ثبت نمایید' onChange={mozoOnchange} required />
                     </div>
 
-                    <div className={classes.control}>
-                        <div className={classes.select1}>
+                    <div className={classes.rightcontrol}>
 
-                            <select onClick={getApiHandler} onChange={(e) => OptionChangeHandler(e)} required>
-                                <option> ------ </option>
+                        <div className={classes.text}>
 
-                                {selectData ? (
-                                    <>
-                                        {selectData.map((item) => (
-                                            <option value={item.id} key={item.id}> {item.name} </option>
-                                        ))}
-                                    </>
-                                ) : ""}
+                            <label>
+                                گروه کالایی
+                                <span>*</span>
+                            </label>
 
+                            <label>
+                                محصول
+                                <span>*</span>
+                            </label>
 
-                            </select>
+                            <label>
+                                ساخت
+                                <span></span>
+                            </label>
 
-                            <select required onChange={categoryOnclick}>
-                                <option value="0">-----</option>
+                            <label>
+                                آدرس
+                                <span></span>
+                            </label>
 
-                                {getCategory ? (
-                                    <>
-                                        {getCategory.map((item) => (
-                                            <option value={item.id} key={item.id}> {item.name} </option>
-                                        ))}
-                                    </>
-                                ) : ""}
-
-                            </select>
-
-                            <select onClick={sakhtDataOnclick} onChange={(e) => sakhtDataOnchange(e)}>
-                                <option>-----</option>
-
-                                {getSakht ? (
-                                    <>
-                                        {getSakht.map((item) => (
-                                            <option value={item.id} key={item.id}> {item.name} </option>
-                                        ))}
-                                    </>
-                                ) : ""}
-
-                            </select>
 
                         </div>
 
-                        <div className={classes.select24}>
-                            <div>
-                                <input type="text" />
-                            </div>
-                        </div>
+                        <div className={classes.control}>
+                            <div className={classes.select1}>
 
+                                <select onClick={getApiHandler} onChange={(e) => OptionChangeHandler(e)} required>
+                                    <option> ------ </option>
 
-                    </div>
-
-                </div>
-
-                <div className={classes.leftcontrol}>
-
-                    <div className={classes.textleft} >
-
-
-                        <label>
-                            برند
-                            <span></span>
-                        </label>
-
-                        <label>
-                            قیمت
-                            <span></span>
-                        </label>
-
-                        <label>
-                            وضعیت
-                            <span>*</span>
-                        </label>
-
-                        <label>
-                            استان
-                            <span></span>
-                        </label>
-
-
-                    </div>
-                    <div className={classes.controlleft}>
-                        <div className={classes.selectleft}>
-                            <div>
-                                <select onClick={brandOnclick} onChange={(e) => brandOnchange(e)}>
-                                    <option>-----</option>
-
-                                    {getBrand ? (
+                                    {selectData ? (
                                         <>
-                                            {getBrand.map((item) => (
+                                            {selectData.map((item) => (
+                                                <option value={item.id} key={item.id}> {item.name} </option>
+                                            ))}
+                                        </>
+                                    ) : ""}
+
+
+                                </select>
+
+                                <select required onChange={categoryOnclick}>
+                                    <option value="0">-----</option>
+
+                                    {getCategory ? (
+                                        <>
+                                            {getCategory.map((item) => (
                                                 <option value={item.id} key={item.id}> {item.name} </option>
                                             ))}
                                         </>
                                     ) : ""}
 
                                 </select>
-                            </div>
 
-
-                            <div>
-                                <div className={classes.select30}>
-                                    <input type="text" placeholder="قیمت را ثبت نمایید" />
-                                    <div className={classes.sel31}>
-                                        <select>
-                                            <option>توافقی</option>
-                                            <option>تومان</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div>
-                                <select required >
-                                    <option value="">-----</option>
-                                    <option>نو</option>
-                                    <option>کارکرده</option>
-                                </select>
-                            </div>
-
-
-
-                            <div>
-                                <select onClick={ostanDataOnclick} onChange={(e) => ostanDataOnchange(e)}>
+                                <select onClick={sakhtDataOnclick} onChange={(e) => sakhtDataOnchange(e)}>
                                     <option>-----</option>
 
-                                    {getOstan ? (
+                                    {getSakht ? (
                                         <>
-                                            {getOstan.map((item) => (
-                                                <option value={item.id} key={item.id}> {item.name_city} </option>
+                                            {getSakht.map((item) => (
+                                                <option value={item.id} key={item.id}> {item.name} </option>
                                             ))}
                                         </>
                                     ) : ""}
 
                                 </select>
+
                             </div>
-                            <div></div>
+
+                            <div className={classes.select24}>
+                                <div>
+                                    <input type="text" />
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                    <div className={classes.leftcontrol}>
+
+                        <div className={classes.textleft} >
+
+
+                            <label>
+                                برند
+                                <span></span>
+                            </label>
+
+                            <label>
+                                قیمت
+                                <span></span>
+                            </label>
+
+                            <label>
+                                وضعیت
+                                <span>*</span>
+                            </label>
+
+                            <label>
+                                استان
+                                <span></span>
+                            </label>
+
+
+                        </div>
+                        <div className={classes.controlleft}>
+                            <div className={classes.selectleft}>
+                                <div>
+                                    <select onClick={brandOnclick} onChange={(e) => brandOnchange(e)}>
+                                        <option>-----</option>
+
+                                        {getBrand ? (
+                                            <>
+                                                {getBrand.map((item) => (
+                                                    <option value={item.id} key={item.id}> {item.name} </option>
+                                                ))}
+                                            </>
+                                        ) : ""}
+
+                                    </select>
+                                </div>
+
+
+                                <div>
+                                    <div className={classes.select30}>
+                                        <input disabled={!showInput} onChange={(e) => priceHandler(e)} type="text" placeholder="قیمت را ثبت نمایید" />
+                                        <div className={classes.sel31}>
+                                            <select onChange={(e) => monayHandler(e)}>
+                                                <option>توافقی</option>
+                                                <option>تومان</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <select required onChange={(e) => statusHandler(e)}>
+                                        <option value="">-----</option>
+                                        <option>نو</option>
+                                        <option>کارکرده</option>
+                                    </select>
+                                </div>
+
+
+
+                                <div>
+                                    <select onClick={ostanDataOnclick} onChange={(e) => ostanDataOnchange(e)}>
+                                        <option>-----</option>
+
+                                        {getOstan ? (
+                                            <>
+                                                {getOstan.map((item) => (
+                                                    <option value={item.id} key={item.id}> {item.name_city} </option>
+                                                ))}
+                                            </>
+                                        ) : ""}
+
+                                    </select>
+                                </div>
+                                <div></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={classes.textarea} >
+                        <div>
+                            <label>
+                                توضیحات <span>*</span>
+                            </label>
+                        </div>
+                        <div className={classes.textarea2}>
+                            <textarea required onChange={(e) => textAreaHandler(e)} rows="12" cols="120" placeholder="توضیحات خود را ثبت نمایید"></textarea>
                         </div>
                     </div>
 
-                </div>
-                <div className={classes.textarea} >
-                    <div>
-                        <label>
-                            توضیحات <span>*</span>
-                        </label>
-                    </div>
-                    <div className={classes.textarea2}>
-                        <textarea rows="12" cols="120" placeholder="توضیحات خود را ثبت نمایید"></textarea>
-                    </div>
-                </div>
+                    <div className='row col-12'>
 
-                <div className='row col-12'>
+                        <div className='col-lg-8'>
+                            <button className='btn btn-success text-white' >ذخیره اطلاعات</button>
+                        </div>
 
-                    <div className='col-lg-8'>
-                        <button className='btn btn-success text-white' onClick={submitDataHandler}>ذخیره اطلاعات</button>
                     </div>
 
+                    {/* <div className={classes.file}>
+        <label>عکس خود را وارد کنید</label>
+        <input type="file" />
+    </div>
+    <div className={classes.btn2}>
+        <button>تصویر فرعی اول </button></div>
+
+    <div className={classes.btn3}><button >تصویر فرعی اول </button></div>
+    <div className={classes.btn4}><button >تصویر فرعی اول </button></div> */}
+
+
+
                 </div>
-
-                {/* <div className={classes.file}>
-                    <label>عکس خود را وارد کنید</label>
-                    <input type="file" />
-                </div>
-                <div className={classes.btn2}>
-                    <button>تصویر فرعی اول </button></div>
-
-                <div className={classes.btn3}><button >تصویر فرعی اول </button></div>
-                <div className={classes.btn4}><button >تصویر فرعی اول </button></div> */}
-
-
-
             </div>
-        </div>
+
+        </form>
 
     );
 };
