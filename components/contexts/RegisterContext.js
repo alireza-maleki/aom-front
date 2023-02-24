ecdda9ea1c15ed049364b78c82756824a20bd814
ali-maleki <alirezamalekinia1111@gmail.com>
2023-02-24 10:49:42 +0330

    component register-code

---
+++ components/contexts/RegisterContext.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+
+
+const RegisterContextApi = React.createContext({
+    isLoggenIn: false,
+    regiserCode: '',
+    onLogOut: () => { },
+    onLogin: (phone) => { },
+    setRegisterCode: (code) => { }
+});
+
+
+export const RegisterContextProvider = (props) => {
+
+    // ### Handler User Loggin / Register ###
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // ### Save Register Code ###
+    const [regiserCode, setRegisterCode] = useState();
+
+    // ===
+    useEffect(() => {
+        // console.log('useEffect - Ctx')
+        // const loginStatus = localStorage.getItem('isLoggedIn');
+
+        // if (loginStatus === "1") {
+        //     setIsLoggedIn(true);
+        // }
+
+    }, []);
+
+    // ===
+    const loginHandler = (phone) => {
+        localStorage.setItem(`isLoggedIn-ctx => ${phone} `, phone);
+
+        console.log("login - ctx")
+        setIsLoggedIn(true);
+    }
+
+    // ===
+    const logoutHandler = () => {
+        localStorage.removeItem("isLoggedIn");
+        setIsLoggedIn(false);
+    }
+
+    // const value = {
+    //     isLoggedIn: isLoggedIn,
+    //     onLogin: loginHandler,
+    //     onLogOut: logoutHandler 
+    // }
+
+    return (
+        <RegisterContextApi.Provider
+            value={{
+                setRegisterCode: setRegisterCode,
+                regiserCode: regiserCode,
+                isLoggedIn: isLoggedIn,
+                onLogOut: logoutHandler,
+                onLogin: loginHandler
+            }}
+        >
+            {props.children}
+        </RegisterContextApi.Provider>
+    )
+
+}
+
+export default RegisterContextApi;
